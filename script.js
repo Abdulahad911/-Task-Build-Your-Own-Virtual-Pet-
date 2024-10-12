f0d935c6be0049b1bfc925d11719f31f9a74d44a
Abdulahad911 <a.a.khan6204@gmail.com>
2024-10-12 09:04:34 -0700

    Adding feed function

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 
 var pet = {
-    name: "Jack",
+    name: "Buddy", 
     type: "dog", 
-    age: 2,
-    happiness: 80,
-    hunger: 40,
+    age: 3,
+    happiness: 70,
+    hunger: 50,
+    feed: function() {
+        this.hunger = this.hunger- 20;
+        if (this.hunger < 0) {
+            this.hunger = 0;
+        }
+        alert(`${this.name} has been fed! Hunger is now: ${this.hunger}`);
+    },
+    
 }
