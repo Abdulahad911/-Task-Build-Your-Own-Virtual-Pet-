656292c39c1795b974530d90b06e844bbf5084e3
Abdulahad911 <a.a.khan6204@gmail.com>
2024-10-13 00:18:28 -0700

    Adding prompt to properties

--- script.js
+++ script.js
@@ -1,47 +1,66 @@
-
+var petName = prompt('What is your pet\'s name?');
+var petType = prompt('What kind of pet\'s it is .e.g. "dog", "cat", "dragon" ?');
+var petAge = +prompt('What is your pet\'s age?Enter a value as number');
+var petHappiness = +prompt('How happy is your pet?Enter a value as number');
+var petHunger = +prompt('How hungry is your pet?Enter a value as number');
 var pet = {
-    name: "Jack", 
-    type: "dog", 
-    age: 2,
-    happiness: 60,
-    hunger: 40,
+    name: "", 
+    type: "", 
+    age: 0,
+    happiness: 0,
+    hunger: 0,
+    
     feed: function() {
-        this.hunger = this.hunger - 20;
+        this.hunger -= 20;
         if (this.hunger < 0) {
             this.hunger = 0;
         }
         alert(`${this.name} has been fed! Hunger is now: ${this.hunger}`);
     },
     
-   play: function(){
-    this.happiness += 20;
-    if (this.happiness > 100){
-        this.happiness = 100;
+    play: function() {
+        this.happiness += 20;
+        if (this.happiness > 100) {
+            this.happiness = 100;
+        }
+        alert(`${this.name} had fun playing! Happiness is now: ${this.happiness}`);
+    },
+    
+    agePet: function() {
+        this.age += 1;
+        this.happiness -= 5;
+        this.hunger += 10;
+        alert(`${this.name} is now ${this.age} years old! Hunger is ${this.hunger}, Happiness is ${this.happiness}`);
     }
-    alert(`${this.name} had fun playing! Happiness is now: ${this.happiness}`);
-   },
-   agePet: function() {
-    this.age += 1;
-    this.happiness -= 5;
-    this.hunger += 10;
-    alert(`${this.name} is now ${this.age} years old! Hunger is ${this.hunger}, Happiness is ${this.happiness}`);
-   } }
-   function petInteraction() {
-    while(true){
+}
+
+function petInteraction() {
+    pet.name = petName;
+    pet.type = petType;
+    pet.age = petAge;
+    pet.happiness = petHappiness;
+    pet.hunger = petHunger;
+
+    while (true) {
         var action = prompt("What would you like to do? (feed/play/age/exit)").toLowerCase();
+        
         if (action === 'feed') {
             pet.feed();
-        }
+        } 
         else if (action === "play") {
             pet.play();
-        } else if (action === "age") {
+        } 
+        else if (action === "age") {
             pet.agePet();
-        } else if (action === "exit") {
+        } 
+        else if (action === "exit") {
             alert("Goodbye!");
             break;
-        } else {
+        } 
+        else {
             alert("Invalid action! Please choose feed, play, age, or exit.");
         }
     }
-    }
+}
+
 petInteraction();
